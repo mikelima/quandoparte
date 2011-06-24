034ea7f79dd95e35d81e92c9d55ad0cc744d743d
Luciano Montanaro <mikelima@cirulla.net>
2011-06-24 02:17:46 +0200

    Added "halts" in addition to stations
    - Modified scripts to separately download data about halts (minor
      stations)
    - Regenerated station list

--- application/resources/stations/downloaddata.sh
+++ application/resources/stations/downloaddata.sh
@@ -1,11 +1,22 @@
 #! /bin/sh
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=8,46,14,47]" -o nord.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=6.7,44,10,46]" -o nordovest.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=10,44,14,46]" -o nordest.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=7.5,43.5,8.5,44]" -o liguria.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=10,42,15,44]" -o centronord.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=12,40,17,42]" -o centrosud.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=12.3,38,17.3,40]" -o sud.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=12,36.5,16.5,38]" -o sicilia.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=17.3,39.5,19,41]" -o puglia.osm
-curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station|halt][bbox=8,38.7,10,41.3]" -o sardegna.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=8,46,14,47]" -o nord.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=6.7,44,10,46]" -o nordovest.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=10,44,14,46]" -o nordest.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=7.5,43.5,8.5,44]" -o liguria.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=10,42,15,44]" -o centronord.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=12,40,17,42]" -o centrosud.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=12.3,38,17.3,40]" -o sud.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=12,36.5,16.5,38]" -o sicilia.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=17.3,39.5,19,41]" -o puglia.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=station][bbox=8,38.7,10,41.3]" -o sardegna.osm
+
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=8,46,14,47]" -o h-nord.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=6.7,44,10,46]" -o h-nordovest.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=10,44,14,46]" -o h-nordest.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=7.5,43.5,8.5,44]" -o h-liguria.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=10,42,15,44]" -o h-centronord.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=12,40,17,42]" -o h-centrosud.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=12.3,38,17.3,40]" -o h-sud.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=12,36.5,16.5,38]" -o h-sicilia.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=17.3,39.5,19,41]" -o h-puglia.osm
+curl --location --globoff "http://jxapi.openstreetmap.org/xapi/api/0.6/node[railway=halt][bbox=8,38.7,10,41.3]" -o h-sardegna.osm
--- application/resources/stations/generatelist.xq
+++ application/resources/stations/generatelist.xq
@@ -1,17 +1,30 @@
 <stations>
 {
-for $doc in ('nord.osm',
-             'nordovest.osm',
-             'nordest.osm',
-             'liguria.osm',
-             'centronord.osm', 
-	     'centrosud.osm', 
-	     'sud.osm', 
-	     'sicilia.osm', 
-	     'puglia.osm', 
-	     'sardegna.osm')
+for $doc in (
+	'nord.osm',
+	'nordovest.osm',
+	'nordest.osm',
+	'liguria.osm',
+	'centronord.osm', 
+	'centrosud.osm', 
+	'sud.osm', 
+	'sicilia.osm', 
+	'puglia.osm', 
+	'sardegna.osm',
+	'h-nord.osm',
+	'h-nordovest.osm',
+	'h-nordest.osm',
+	'h-liguria.osm',
+	'h-centronord.osm', 
+	'h-centrosud.osm', 
+	'h-sud.osm', 
+	'h-sicilia.osm', 
+	'h-puglia.osm', 
+	'h-sardegna.osm')
 for $node in doc($doc)//node
 where $node/tag[@k='name'] and (
+fn:not(fn:exists($node//tag[@k='disused'])) or fn:contains($node//tag[@k='disused']/@v, 'no') 
+) and (
 $node/tag[@k='operator'] and (
 fn:contains($node//tag[@k='operator']/@v, 'RFI') or 
 fn:contains($node//tag[@k='operator']/@v, 'Centostazioni') or 
