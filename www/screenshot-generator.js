24d2e8acd5619b9c605d77c520bcd87f3573e1a2
Luciano Montanaro <mikelima@cirulla.net>
2011-06-30 01:07:43 +0200

    Added javascript screenshot generator
    Forgot to add in latest commit

---
+++ www/screenshot-generator.js
@@ -0,0 +1,34 @@
+var screenshots = {
+	"screenshot-stationlist.png" : "The Station List View",
+	"screenshot-stationview.png" : "The Station Departures View",
+};
+
+function generate_div(value, index, array)
+{
+	var screenshots = document.getElementById('screenshot-list');
+	var screenshotDiv = document.createElement('div');
+	screenshotDiv.setAttribute('class', 'screenshot');
+	
+	var p1Element = document.createElement('div');
+	var p2Element = p1Element.cloneNode(true);
+	
+	var imgElement = document.createElement('img');
+	imgElement.setAttribute('src', index);
+
+	var captionElement = document.createTextNode(value);
+
+	p1Element.appendChild(imgElement);
+	p2Element.appendChild(captionElement);
+
+	screenshotDiv.appendChild(p1Element);
+	screenshotDiv.appendChild(p2Element);
+	screenshots.appendChild(screenshotDiv);
+}
+
+function build_screenshot_list()
+{
+	for (var key in screenshots)
+		generate_div(screenshots[key], key, screenshots);
+}
+
+window.addEventListener('load', build_screenshot_list);
