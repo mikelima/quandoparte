bc588bdf23acccc277043fdc5e74afbf68a9aad9
Luciano Montanaro <mikelima@cirulla.net>
2011-08-09 14:49:34 +0200

    Use accent color for search highlight

--- application/resources/harmattan/qml/StationListPage.js
+++ application/resources/harmattan/qml/StationListPage.js
@@ -14,14 +14,15 @@ function loadStation(name)
         console.log('Cannot load component: ' + component.errorString());
 }
 
-function highlightSearch(s)
+function highlightSearch(s, color)
 {
     // TODO compile RegExp on change, or find a way to cleanly use
     // stationListProxyModel.filterRegExp
+    console.log("string:" + s + " color: " + color)
     if (searchField.text.length) {
         var r = new RegExp(searchField.text, 'i')
         var match = r.exec(s)
-        return s.replace(r, '<span style="text-decoration:underline">' +
+        return s.replace(r, '<span style="text-decoration:underline;color:' + color + ';">' +
                          match + '</span>')
     } else {
         return s
