18ad9e7e1326982606e65f66974504b4b48d4004
Luciano Montanaro <mikelima@cirulla.net>
2011-11-20 16:32:53 +0100

    Split out StationScheduleItem to its own file set

---
+++ application/stationscheduleitem.cpp
@@ -0,0 +1,135 @@
+/*
+
+Copyright (C) 2011 mikelima
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+
+*/
+
+#include "stationscheduleitem.h"
+#include <QSharedData>
+#include <QString>
+
+class StationScheduleItemData : public QSharedData {
+public:
+    QString id;
+    QString departureStation;
+    QString departureTime;
+    QString arrivalStation;
+    QString arrivalTime;
+    QString detailsUrl;
+    QString delay;
+    int delayClass;
+};
+
+StationScheduleItem::StationScheduleItem() : d(new StationScheduleItemData)
+{
+}
+
+StationScheduleItem::StationScheduleItem(const StationScheduleItem &rhs) : d(rhs.d)
+{
+}
+
+StationScheduleItem &StationScheduleItem::operator=(const StationScheduleItem &rhs)
+{
+    if (this != &rhs)
+        d.operator=(rhs.d);
+    return *this;
+}
+
+StationScheduleItem::~StationScheduleItem()
+{
+}
+
+QString &StationScheduleItem::id()
+{
+    return d->id;
+}
+
+void StationScheduleItem::setId(const QString &value)
+{
+    d->id = value;
+}
+
+QString &StationScheduleItem::departureStation()
+{
+    return d->departureStation;
+}
+
+void StationScheduleItem::setDepartureStation(const QString &value)
+{
+    d->departureStation = value;
+}
+
+QString &StationScheduleItem::departureTime()
+{
+    return d->departureTime;
+}
+
+void StationScheduleItem::setDepartureTime(const QString &value)
+{
+    d->departureTime = value;
+}
+
+QString &StationScheduleItem::arrivalStation()
+{
+    return d->arrivalStation;
+}
+
+void StationScheduleItem::setArrivalStation(const QString &value)
+{
+    d->arrivalStation = value;
+}
+
+QString &StationScheduleItem::arrivalTime()
+{
+    return d->arrivalTime;
+}
+
+void StationScheduleItem::setArrivalTime(const QString &value)
+{
+    d->arrivalTime = value;
+}
+
+QString &StationScheduleItem::detailsUrl()
+{
+    return d->detailsUrl;
+}
+
+void StationScheduleItem::setDetailsUrl(const QString &value)
+{
+    d->detailsUrl = value;
+}
+
+QString &StationScheduleItem::delay()
+{
+    return d->delay;
+}
+
+void StationScheduleItem::setDelay(const QString &value)
+{
+    d->delay = value;
+}
+
+int StationScheduleItem::delayClass()
+{
+    return d->delayClass;
+}
+
+void StationScheduleItem::setDelayClass(const int value)
+{
+    d->delayClass = value;
+}
---
+++ application/stationscheduleitem.h
@@ -0,0 +1,66 @@
+/*
+
+Copyright (C) 2011 mikelima
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+
+*/
+
+#ifndef STATIONSCHEDULEITEM_H
+#define STATIONSCHEDULEITEM_H
+
+#include <QSharedDataPointer>
+
+class StationScheduleItemData;
+
+class StationScheduleItem
+{
+public:
+    StationScheduleItem();
+    StationScheduleItem(const StationScheduleItem &);
+    StationScheduleItem &operator=(const StationScheduleItem &);
+    ~StationScheduleItem();
+
+    QString &id();
+    void setId(const QString &value);
+
+    QString &departureStation();
+    void setDepartureStation(const QString &value);
+
+    QString &departureTime();
+    void setDepartureTime(const QString &value);
+
+    QString &arrivalStation();
+    void setArrivalStation(const QString &value);
+
+    QString &arrivalTime();
+    void setArrivalTime(const QString &value);
+
+    QString &detailsUrl();
+    void setDetailsUrl(const QString &value);
+
+    QString &delay();
+    void setDelay(const QString &value);
+
+    int delayClass();
+    void setDelayClass(const int value);
+
+    bool isValid();
+private:
+    QSharedDataPointer<StationScheduleItemData> d;
+};
+
+#endif // STATIONSCHEDULEITEM_H
