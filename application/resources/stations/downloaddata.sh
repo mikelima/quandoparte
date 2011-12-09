39bf562a5927ffcd6e49fb56bf3c1b5607e3c0fd
Luciano Montanaro <mikelima@cirulla.net>
2011-12-09 21:09:01 +0100

    Add new faster data source
    to the openstreetmap data downloader script
    
    Conflicts:
    
    	application/resources/stations/downloaddata.sh

--- application/resources/stations/downloaddata.sh
+++ application/resources/stations/downloaddata.sh
@@ -1,28 +1,30 @@
 #! /bin/sh
-BASEURL=http://jxapi.openstreetmap.org/xapi/api/0.6
-#BASEURL=http://open.mapquestapi.com/xapi/api/0.6
+#BASEURL=http://jxapi.openstreetmap.org/xapi/api/0.6/
+#BASEURL=http://open.mapquestapi.com/xapi/api/0.6/
+BASEURL=http://www.overpass-api.de/api/xapi?
 
-curl --location --globoff "$BASEURL/node[railway=station][bbox=8,46,14,47]" -o nord.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=6.7,44,10,46]" -o nordovest.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=10,44,14,46]" -o nordest.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=7.5,43.5,8.5,44]" -o liguria.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=10,42,15,44]" -o centronord.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=12,40,17,42]" -o centrosud.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=12.3,38,17.3,40]" -o sud.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=12,36.5,16.5,38]" -o sicilia.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=17.3,39.5,19,41]" -o puglia.osm
-curl --location --globoff "$BASEURL/node[railway=station][bbox=8,38.7,10,41.3]" -o sardegna.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=8,46,14,47]" -o nord.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=6.7,44,10,46]" -o nordovest.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=10,44,14,46]" -o nordest.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=7.5,43.5,8.5,44]" -o liguria.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=10,42,15,44]" -o centronord.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=12,40,17,42]" -o centrosud.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=12.3,38,17.3,40]" -o sud.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=12,36.5,16.5,38]" -o sicilia.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=17.3,39.5,19,41]" -o puglia.osm
+curl --location --globoff $BASEURL"node[railway=station][bbox=8,38.7,10,41.3]" -o sardegna.osm
 
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=8,46,14,47]" -o h-nord.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=6.7,44,10,46]" -o h-nordovest.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=10,44,14,46]" -o h-nordest.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=7.5,43.5,8.5,44]" -o h-liguria.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=10,42,15,44]" -o h-centronord.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=12,40,17,42]" -o h-centrosud.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=12.3,38,17.3,40]" -o h-sud.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=12,36.5,16.5,38]" -o h-sicilia.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=17.3,39.5,19,41]" -o h-puglia.osm
-curl --location --globoff "$BASEURL/node[railway=halt][bbox=8,38.7,10,41.3]" -o h-sardegna.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=8,46,14,47]" -o h-nord.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=6.7,44,10,46]" -o h-nordovest.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=10,44,14,46]" -o h-nordest.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=7.5,43.5,8.5,44]" -o h-liguria.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=10,42,15,44]" -o h-centronord.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=12,40,17,42]" -o h-centrosud.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=12.3,38,17.3,40]" -o h-sud.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=12,36.5,16.5,38]" -o h-sicilia.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=17.3,39.5,19,41]" -o h-puglia.osm
+curl --location --globoff $BASEURL"node[railway=halt][bbox=8,38.7,10,41.3]" -o h-sardegna.osm
+exit 0
 
 curl --location --globoff "$BASEURL/way[railway=station][bbox=8,46,14,47]" -o w-nord.osm
 curl --location --globoff "$BASEURL/way[railway=station][bbox=6.7,44,10,46]" -o w-nordovest.osm
